97b959a00ffac42938b266580970080f365e3776
hildelio <hildelio@gmail.com>
2023-10-21 20:19:40 -0300

    Projeto Kotlin Playground I :national_park: :roller_coaster: :carousel_horse: :ferris_wheel: Req 06

--- src/main/kotlin/Req06.kt
+++ src/main/kotlin/Req06.kt
@@ -1,7 +1,24 @@
 fun calculateNewSalary(currentSalary: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    return if (currentSalary <= 300.0) {
+        currentSalary * 1.5
+    } else if (currentSalary > 300 && currentSalary <= 500.0) {
+        currentSalary * 1.4
+    } else if (currentSalary > 500.0 && currentSalary <= 700.0) {
+        currentSalary * 1.3
+    } else if (currentSalary > 700.0 && currentSalary <= 800.0) {
+        currentSalary * 1.2
+    } else if (currentSalary > 800.0 && currentSalary <= 1000.0) {
+        currentSalary * 1.1
+    } else {
+        currentSalary * 1.05
+    }
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+    calculateNewSalary(200.0)
+    calculateNewSalary(400.0)
+    calculateNewSalary(600.0)
+    calculateNewSalary(750.0)
+    calculateNewSalary(900.0)
+    calculateNewSalary(1200.0)
 }
