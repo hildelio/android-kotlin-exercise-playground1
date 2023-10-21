809720f1dbee1f451480f5e275fb46b6ff8c1b9b
hildelio <hildelio@gmail.com>
2023-10-21 19:28:47 -0300

    Projeto Kotlin Playground I :national_park: :roller_coaster: :carousel_horse: :ferris_wheel: Req 03

--- src/main/kotlin/Req03.kt
+++ src/main/kotlin/Req03.kt
@@ -1,19 +1,36 @@
 fun calculateYears(birthYear: Int, currentYear: Int): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+    return  currentYear - birthYear
 }
 
 fun calculateMonths(birthYear: Int, currentYear: Int): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+    val years = calculateYears(birthYear, currentYear)
+    val monthsOfYear = 12
+    return years * monthsOfYear
 }
 
 fun calculateDays(birthYear: Int, currentYear: Int): Long {
-    TODO("Use essa função para desenvolver sua lógica")
+    val years = calculateYears(birthYear, currentYear)
+    val daysOfYear = 365.24242424242425
+    return (years * daysOfYear).toLong()
 }
 
 fun calculateWeeks(birthYear: Int, currentYear: Int): Long {
-    TODO("Use essa função para desenvolver sua lógica")
+    val days = calculateDays(birthYear, currentYear)
+    val daysOfWeek = 7
+    return days / daysOfWeek
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+    val birthYear = 1990
+    val currentYear = 2023
+
+    val years = calculateYears(birthYear, currentYear)
+    val months = calculateMonths(birthYear, currentYear)
+    val days = calculateDays(birthYear, currentYear)
+    val weeks = calculateWeeks(birthYear, currentYear)
+
+    println("Idade em anos: $years")
+    println("Idade em meses: $months")
+    println("Idade em dias: $days")
+    println("Idade em semanas: $weeks")
 }
