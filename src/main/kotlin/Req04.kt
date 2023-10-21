17e26e3b71df9ae16f7154d513363ba235ec52d8
hildelio <hildelio@gmail.com>
2023-10-21 19:48:44 -0300

    Projeto Kotlin Playground I :national_park: :roller_coaster: :carousel_horse: :ferris_wheel: Req 04

--- src/main/kotlin/Req04.kt
+++ src/main/kotlin/Req04.kt
@@ -1,19 +1,32 @@
 fun calculateAverage(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    return (number1 + number2) / 2
 }
 
 fun calculateDifference(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    return if (number1 > number2) {
+        number1 - number2
+    } else {
+        number2 - number1
+    }
 }
 
 fun calculateProduct(number1: Double, number2: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    return number1 * number2
 }
 
 fun calculateDivision(number1: Double, number2: Double): Double? {
-    TODO("Use essa função para desenvolver sua lógica")
+    return if (number2 == 0.0) {
+        null
+    } else {
+        number1 / number2
+    }
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+    calculateAverage(10.0, 20.0)
+    calculateDifference(20.0, 10.0)
+    calculateDifference(20.0, 10.0)
+    calculateProduct(5.0, 4.0)
+    calculateDivision(10.0, 2.0)
+    calculateDivision(10.0, 0.0)
 }
