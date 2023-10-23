6a15512dbcce7c8846efe4aeab7795d6147948fb
hildelio <hildelio@gmail.com>
2023-10-23 14:24:26 -0300

    Projeto Kotlin Playground I :national_park: :roller_coaster: :carousel_horse: :ferris_wheel: Req 12 fixed :detective:

--- src/main/kotlin/Req12.kt
+++ src/main/kotlin/Req12.kt
@@ -1,17 +1,16 @@
 fun multiplyLists(list1: List<Int>, list2: List<Int>, result: MutableList<Int>) {
-    if (list1.size != list2.size || list1.size != result.size) {
-        throw IllegalArgumentException("As listas devem ter o mesmo tamanho")
-    }
+    require(list1.size == list2.size) { "As listas devem ter o mesmo tamanho" }
 
     for (i in list1.indices) {
-        result[i] = list1[i] * list2[i]
+        result.add(list1[i] * list2[i])
     }
 }
 
 fun main() {
     val list1 = List(10) { it + 1}
     val list2 = (10 downTo 1).toList()
-    val result = MutableList(list1.size) { 0 }
+    val result = mutableListOf<Int>()
+
     multiplyLists(list1, list2, result)
     println(result)
 }
